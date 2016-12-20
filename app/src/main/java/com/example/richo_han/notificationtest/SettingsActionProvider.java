48dca0606d9ac0190b189aa9682440fa8ce4e1e9
Richo_Han <Richo_Han@asus.com>
2016-12-20 11:57:19 +0800

    Configure class path.

--- app/src/main/java/com/example/richo_han/notificationtest/SettingsActionProvider.java
+++ app/src/main/java/com/example/richo_han/notificationtest/SettingsActionProvider.java
@@ -2,6 +2,7 @@ package com.example.richo_han.notificationtest;
 
 import android.content.Context;
 import android.support.v4.view.ActionProvider;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -20,6 +21,7 @@ public class SettingsActionProvider extends ActionProvider {
 
     @Override
     public View onCreateActionView() {
+        Log.d("Tag", "ActionProvider loading.");
         return null;
     }
 }
